010cd6129c727c298c8431c6f31b77c76e504dbb
ValentinKolesnikov <behumbleplez@gmail.com>
2021-01-12 15:58:26 +0300

    Algorithm has been simplified with help of inbuilt Counter method most_common()

--- most_common_words/main.py
+++ most_common_words/main.py
@@ -12,16 +12,8 @@ def count_most_common_words(text, words_count):
     pattern = re.compile(r'[-.?!,/()\'\":;]')
     formated_text = re.sub(pattern, '', text)
 
-    all_words_list = formated_text.split()
-    unique_words_list = list(set(all_words_list))
-    words_counter = collections.Counter(all_words_list)
-    words_frequency = {word:words_counter[word] for word in unique_words_list}
-    words_frequency = list(words_frequency.items())
-
-    sorted_words_frequency = sort_words_by_frequency(words_frequency)
-
-    return sorted_words_frequency[:words_count]
-
+    words_counter = collections.Counter(formated_text.split())
+    return words_counter.most_common(words_count)
 
 def print_results(words_frequency):
     for item in words_frequency:
@@ -33,8 +25,7 @@ if __name__ == '__main__':
     parser.add_argument('file', metavar='Path', nargs='?', type=argparse.FileType('r'), default=sys.stdin, help='path to file that will parse')
     parser.add_argument('number_to_display', metavar='N', nargs='?', type=int, help='number of the most common words to display')
     
-    args = parser.parse_args(['./text.txt', '10'])
-    # args = parser.parse_args()
+    args = parser.parse_args()
 
     text = args.file.read().lower()
     args.file.close()
