da9a6fb39f1d17e1cf57f4f8a5c82292103ad50b
ValentinKolesnikov <behumbleplez@gmail.com>
2021-01-14 13:49:47 +0300

    Fix some small issues

--- python_upcoming-events/main.py
+++ python_upcoming-events/main.py
@@ -4,11 +4,19 @@ from datetime import datetime as dt
 from collections import namedtuple
 import threading
 import concurrent.futures
+from operator import attrgetter
+
+
+Event = namedtuple('Event', 'title location date_start date_end')
+
+
+def format_date_for_output(date):
+    return date.strftime('%d %B %Y')
 
 
 def reformat_event_date(date):
     reformat_date = dt.strptime(date, '%Y-%m-%dT%H:%M:%S%z')
-    return reformat_date.strftime('%d %B %Y')
+    return reformat_date
 
 def get_events_links(main_link):
     soup = get_soup(main_link)
@@ -18,36 +26,41 @@ def get_events_links(main_link):
 
     return [python_main_link + event_link['href'] for event_link in soup.select('h3.event-title a')]
 
+
 def get_event_info(link):
     soup = get_soup(link)
-    return [soup.select_one('h1.single-event-title').text.strip(),
-            soup.select_one('span.single-event-location').text.strip(),
-            reformat_event_date(soup.select_one('time.date-start')['datetime']),
-            reformat_event_date(soup.select_one('time.date-end')['datetime'])]
+
+    return Event(soup.select_one('h1.single-event-title').text.strip(),
+                    soup.select_one('span.single-event-location').text.strip(),
+                    reformat_event_date(soup.select_one('time.date-start')['datetime']),
+                    reformat_event_date(soup.select_one('time.date-end')['datetime']))
+
 
 def get_events_info(main_link):
     events_links = get_events_links(main_link)
     events_info = []
-    EventInfo = namedtuple('EventInfo', 'title location date_start date_end')
    
     with concurrent.futures.ThreadPoolExecutor(max_workers=len(events_links)) as executor:
         futures = [executor.submit(get_event_info, link) for link in events_links]
 
         for future in concurrent.futures.as_completed(futures):
-            events_info.append(EventInfo(*future.result()))
-    
+            events_info.append(future.result())
+    events_info.sort(key=attrgetter('date_start'))
     return events_info
+
 
 def get_soup(link):
     response = requests.get(link)
     return BeautifulSoup(response.text, 'lxml')
 
+
 def print_events(events_info):
     for event_info in events_info:
         print(f'\nEvent title: {event_info.title}\n'
-              f'Date start: {event_info.date_start}\n'
-              f'Date end: {event_info.date_end}\n'
+              f'Date start: {format_date_for_output(event_info.date_start)}\n'
+              f'Date end: {format_date_for_output(event_info.date_end)}\n'
               f'Location: {event_info.location}')
+
 
 if __name__ == '__main__':
     python_main_link = 'https://www.python.org'
