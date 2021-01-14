c37b45acfea05019f7147315fe6c8ffb8b8a2f75
ValentinKolesnikov <behumbleplez@gmail.com>
2021-01-14 11:48:11 +0300

    Rework function of getting events links
     - Change usual request to site to request with help of threads.
    It allowed to reduce the time of access and data acqusition.
    
     - Fix problem with overflowing of "if '__name__' == '__main'".
    
     - Change storage of events info. Dict was replaced by namedtuple.
     It allowed to make code more readable and convenient.

--- python_upcoming-events/main.py
+++ python_upcoming-events/main.py
@@ -1,22 +1,46 @@
 from bs4 import BeautifulSoup
 import requests
 from datetime import datetime as dt
+from collections import namedtuple
+import threading
+import concurrent.futures
+
 
 def reformat_event_date(date):
     reformat_date = dt.strptime(date, '%Y-%m-%dT%H:%M:%S%z')
     return reformat_date.strftime('%d %B %Y')
 
-def get_events_info(links):
-    events_soups = [get_soup(link) for link in links]
-    events_info = []
-    for soup in events_soups:
-        events_info.append({'title': soup.select_one('h1.single-event-title').text.strip(),
-                            'location': soup.select_one('span.single-event-location').text.strip(),
-                            'date_start': reformat_event_date(soup.select_one('time.date-start')['datetime']),
-                            'date_end': reformat_event_date(soup.select_one('time.date-end')['datetime'])
-                            })
 
+def get_events_links(main_link):
+    soup = get_soup(main_link)
+    events_btn = soup.find('li', attrs={'id': 'events'}).a
+    python_events_link = main_link + events_btn['href']
+    soup = get_soup(python_events_link)
+
+    return [python_main_link + event_link['href'] for event_link in soup.select('h3.event-title a')]
+
+
+def get_event_info(link):
+    soup = get_soup(link)
+    return [soup.select_one('h1.single-event-title').text.strip(),
+            soup.select_one('span.single-event-location').text.strip(),
+            reformat_event_date(soup.select_one('time.date-start')['datetime']),
+            reformat_event_date(soup.select_one('time.date-end')['datetime'])]
+
+
+def get_events_info(main_link):
+    events_links = get_events_links(main_link)
+    events_info = []
+    EventInfo = namedtuple('EventInfo', 'title location date_start date_end')
+   
+    with concurrent.futures.ThreadPoolExecutor(max_workers=len(events_links)) as executor:
+        futures = [executor.submit(get_event_info, link) for link in events_links]
+
+        for future in concurrent.futures.as_completed(futures):
+            events_info.append(EventInfo(*future.result()))
+    
     return events_info
+
 
 def get_soup(link):
     response = requests.get(link)
@@ -25,21 +49,13 @@ def get_soup(link):
 
 def print_events(events_info):
     for event_info in events_info:
-        print(f'\nEvent title: {event_info["title"]}\n'
-              f'Date start: {event_info["date_start"]}\n'
-              f'Date end: {event_info["date_end"]}\n'
-              f'Location: {event_info["location"]}')
+        print(f'\nEvent title: {event_info.title}\n'
+              f'Date start: {event_info.date_start}\n'
+              f'Date end: {event_info.date_end}\n'
+              f'Location: {event_info.location}')
+
 
 if __name__ == '__main__':
     python_main_link = 'https://www.python.org'
-    soup = get_soup(python_main_link)
-    events_btn = soup.find('li', attrs={'id': 'events'}).a
-
-    python_events_link = python_main_link + events_btn['href']
-    soup = get_soup(python_events_link)
-
-    events_links = [python_main_link + event_link['href'] for event_link in soup.select('h3.event-title a')]
-
-
-    events_info = get_events_info(events_links)
+    events_info = get_events_info(python_main_link)
     print_events(events_info[:3])
