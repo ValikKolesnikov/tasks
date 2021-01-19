6d0411f3bb1bbdedab62e55a8257b70ae56f2593
ValikKolesnikov <behumbleplez@gmail.com>
2021-01-19 15:17:11 +0300

    Fix problem with booking

--- hotel_reservation_system/hotel_system.py
+++ hotel_reservation_system/hotel_system.py
@@ -6,7 +6,10 @@ class RoomIsNotBookableError(Exception):
     pass
 
 
-class RoomIsNotExistInHotelError(Exception):
+class RoomDoesNotExistInHotelError(Exception):
+    pass
+
+class OrderDoesNotExistError(Exception):
     pass
 
 
@@ -33,18 +36,16 @@ class Hotel:
     def add_rooms(self, rooms):
         self.rooms.extend(rooms)
 
-    def release_room(self, room):
+    def release_room(self, room, order):
         if room in self.rooms:
-            room.tenant = None
-            room.release_date = None
-            print(f'Room #{room.number} is free')
+            room.orders.remove(order)
+            print(f'{order.person.name} is gone room #{room.number}')
         else:
-            raise RoomIsNotExistInHotelError()
+            raise RoomDoesNotExistInHotelError()
 
     def show_price_list(self):
         for room in self.rooms:
             print(f'Room #{room.number}\n'
-                  f'Is free: {room.is_free()}\n'
                   f'Cost: ${room.cost}/day\n')
 
     def get_room_by_number(self, number):
@@ -53,52 +54,59 @@ class Hotel:
                 return room
         return None
 
+    def can_allocate_room(self, person, room, days_count, date_start, date_end):
+        return days_count * room.cost <= person.money and room in self.rooms and room.is_free(date_start, date_end)
+
     def allocate_room(self, person, room, date_start, date_end):
         days_count = (date_end - date_start).days
-        if days_count * room.cost < person.money and room in self.rooms and room.is_free():
-            room.tenant = person
-            room.release_date = date_end
+        if self.can_allocate_room(person, room, days_count, date_start, date_end):
             person.money -= days_count * room.cost
-            print('Congratulations! Room is booked')
+            room.orders.append(Order(person=person, date_start=date_start, date_end=date_end))
+            print(f'Congratulations! Room #{room.number} is booked by {person.name}\n'
+                  f'From {date_start} to {date_end}')
         else:
-            raise RoomIsNotBookableError('You can not book this room(')
+            raise RoomIsNotBookableError(f'You can not book room #{room.number}')
 
 
 class Room:
     def __init__(self, number, cost):
         self.__number = number
         self.__cost = cost
-        self.__tenant = None
-        self.__release_date = None
+        self.__orders = []
 
-    def is_free(self):
-        if self.release_date is None:
-            return True
-        return self.release_date < datetime.now()
+    @property
+    def orders(self):
+        return self.__orders
+
+    @orders.setter
+    def orders(self, orders):
+        self.__orders = orders
 
     @property
     def number(self):
         return self.__number
 
     @property
-    def tenant(self):
-        return self.__tenant
-
-    @tenant.setter
-    def tenant(self, tenant):
-        self.__tenant = tenant
-
-    @property
     def cost(self):
         return self.__cost
 
-    @property
-    def release_date(self):
-        return self.__release_date
+    def is_date_between(self, date, date_start, date_end):
+        return date_start <= date <= date_end
 
-    @release_date.setter
-    def release_date(self, date):
-        self.__release_date = date
+    def is_free(self, date_start, date_end):
+        for order in self.orders:
+            if self.is_date_between(date_start, order.date_start, order.date_end) or self.is_date_between(date_end,
+                                                                                                          order.date_start,
+                                                                                                          order.date_end):
+                return False
+        return True
+
+    def get_order(self, person, date_start, date_end):
+        for order in self.orders:
+            if order.person == person and order.date_start == date_start and order.date_end == date_end:
+                return order
+            raise OrderDoesNotExistError()
+    
 
 
 class Person:
@@ -117,6 +125,37 @@ class Person:
     @property
     def name(self):
         return self.__name
+
+
+class Order:
+    def __init__(self, person, date_start, date_end):
+        self.__person = person
+        self.__date_start = date_start
+        self.__date_end = date_end
+
+    @property
+    def person(self):
+        return self.__person
+
+    @person.setter
+    def person(self, person):
+        self.__person = person
+
+    @property
+    def date_start(self):
+        return self.__date_start
+
+    @date_start.setter
+    def date_start(self, date):
+        self.__date_start = date
+
+    @property
+    def date_end(self):
+        return self.__date_end
+
+    @date_end.setter
+    def date_end(self, date):
+        self.__date_end = date
 
 
 def generate_rooms(count):
--- hotel_reservation_system/main.py
+++ hotel_reservation_system/main.py
@@ -4,17 +4,19 @@ from datetime import datetime
 
 def main():
     hotel = hotel_system.Hotel(name='Tourist', rooms=[])
-    person_1 = hotel_system.Person(name='John', money=3000)
-    person_2 = hotel_system.Person(name='Mike', money=3000)
+    person_1 = hotel_system.Person(name='John', money=300000000)
+    person_2 = hotel_system.Person(name='Mike', money=300000000)
+    person_3 = hotel_system.Person(name='Dan', money=300000000)
 
-    hotel.add_rooms(hotel_system.generate_rooms(count=20))
+    hotel.add_rooms(hotel_system.generate_rooms(count=5))
     hotel.show_price_list()
     room = hotel.get_room_by_number(number=3)
-    hotel.allocate_room(person=person_1, room=room, date_start=datetime(2021, 1, 18), date_end=datetime(2021, 1, 30))
-    hotel.allocate_room(person=person_2, room=room, date_start=datetime(2021, 1, 18), date_end=datetime(2021, 1, 30))
-    hotel.release_room(room=room)
-    hotel.allocate_room(person=person_2, room=room, date_start=datetime(2021, 1, 18), date_end=datetime(2021, 1, 30))
-
+    hotel.allocate_room(person=person_1, room=room, date_start=datetime(2021, 1, 1), date_end=datetime(2021, 1, 5))
+    hotel.allocate_room(person=person_2, room=room, date_start=datetime(2021, 1, 6), date_end=datetime(2021, 1, 12))
+    hotel.allocate_room(person=person_3, room=room, date_start=datetime(2021, 1, 11), date_end=datetime(2021, 1, 13))
+    order = room.get_order(person=person_1, date_start=datetime(2021, 1, 1), date_end=datetime(2021, 1, 5))
+    hotel.release_room(room=room, order=order)
+    
 
 if __name__ == '__main__':
     main()
